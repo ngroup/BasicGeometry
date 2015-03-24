0686f2cc3c97a02dfa15f27a6a548a61c3eef6e5
ngroup <steven.nien@gmail.com>
2015-03-24 14:39:11 +0800

    [fix] remove unwated print added in developing

--- whichside/line.py
+++ whichside/line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class Line:
+class Line(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
@@ -9,11 +9,8 @@ class Line:
         '''
         Compute the cross product of (AB) X (AP), where P is (x, y)
         '''
-
         ab = (self.end[0] - self.start[0], self.end[1] - self.start[1])
         ap = (x - self.start[0], y - self.start[1])
         cp = ab[0] * ap[1] - ap[0] * ab[1]
-        print ab, ap
-        print cp
 
         return cp > 0
--- whichside/polygon.py
+++ whichside/polygon.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-class Polygon:
+class Polygon(object):
     def __init__(self):
         self.vertices = []
 
