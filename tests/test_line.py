2f22609a1dbb20acd0471f47819ac95627e0f2b9
Chun Nien <steven.nien@gmail.com>
2017-06-18 12:13:00 +0800

    add: InfiniteLine

--- tests/test_line.py
+++ tests/test_line.py
@@ -1,17 +1,25 @@
-from tiny2d.line import Line
+from tiny2d import Line, InfiniteLine
 
 
-class TestLine(unittest.TestCase):
-    def setUp(self):
-        pass
+# class TestLine():
+#     def setUp(self):
+#         pass
 
-    def test_line1(self):
-        line = Line([1, 1], [2, 2.5])
-        self.assertFalse(line.is_clockwise_to_point(1, 0))
-        self.assertTrue(line.is_clockwise_to_point(1.5, 3.0))
+#     def test_line1(self):
+#         line = Line([1, 1], [2, 2.5])
+#         self.assertFalse(line.is_clockwise_to_point(1, 0))
+#         self.assertTrue(line.is_clockwise_to_point(1.5, 3.0))
 
-    def test_line2(self):
-        line = Line([1.0, 1.0], [-2.0, -2.5])
-        self.assertFalse(line.is_clockwise_to_point(0.0, 2.0))
-        self.assertTrue(line.is_clockwise_to_point(0, -2))
+#     def test_line2(self):
+#         line = Line([1.0, 1.0], [-2.0, -2.5])
+#         self.assertFalse(line.is_clockwise_to_point(0.0, 2.0))
+#         self.assertTrue(line.is_clockwise_to_point(0, -2))
 
+def test_InfiniteLine_distance():
+    line = InfiniteLine(1, 0, -5)
+    assert line.distance((0, 0)) == 5
+
+def test_InfiniteLine_reflect():
+    line = InfiniteLine(1, 0, -5)
+    assert line.reflect((0, 0)) == (10, 0)
+    assert line.reflect((10, 0)) == (0, 0)
--- tiny2d/line.py
+++ tiny2d/line.py
@@ -1,3 +1,4 @@
+import tiny2d as t2d
 
 class Line(object):
     def __init__(self, start, end):
@@ -13,3 +14,42 @@ class Line(object):
         cp = ab[0] * ap[1] - ap[0] * ab[1]
 
         return cp > 0
+
+
+class InfiniteLine:
+    '''
+    An Infinite Line: a * x + b * y + c = 0
+    '''
+    def __init__(self, a, b, c):
+        self._a = a
+        self._b = b
+        self._c = c
+        self._n_vector = self.n_vector
+        self._reference = self.reference
+
+    @property
+    def n_vector(self):
+        return t2d.Vector2D(self._a, self._b)
+
+    @property
+    def reference(self):
+        '''
+        get a reference point on line
+        '''
+        if self._a == 0:
+            return t2d.Vector2D(0, -1 * sel._c / self._b)
+        else:
+            return t2d.Vector2D(-1 * self._c / self._a, 0)
+
+    def reflect(self, p):
+        p = t2d.Vector2D(*p)
+        rp = p - self._reference
+        u = rp.project_to(self._n_vector)
+        new_p = p - 2 * u
+        return new_p
+
+    def distance(self, p):
+        p = t2d.Vector2D(*p)
+        rp = p - self._reference
+        u = rp.project_to(self._n_vector)
+        return u.length
